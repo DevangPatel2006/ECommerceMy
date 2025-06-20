211ae639b80938508a34246811df55990611c6a7
Devang Patel <girapatel361@gmail.com>
2025-06-20 12:51:26 +0530

    Myntra Clone

---
+++ script.js
@@ -0,0 +1,120 @@
+const products = [
+  {
+    name: "Men's T-Shirt",
+    price: "₹499",
+    img: "https://assets.myntassets.com/f_webp,w_140,c_limit,fl_progressive,dpr_2.0/assets/images/2022/5/2/f7575784-edbf-411f-acc0-51891ea7f4331651484798329-Inddus-_Varanga.jpg"
+  },
+  {
+    name: "Women Kurti",
+    price: "₹799",
+    img: "https://assets.myntassets.com/f_webp,w_140,c_limit,fl_progressive,dpr_2.0/assets/images/2022/5/2/5d1b7ad3-c3ed-4ef9-a654-18231743d3cd1651484798059-Anouk-Inddus.jpg"
+  },
+  {
+    name: "Men's Sneakers",
+    price: "₹1999",
+    img: "https://assets.myntassets.com/f_webp,w_140,c_limit,fl_progressive,dpr_2.0/assets/images/2022/5/2/ce40419d-6408-404e-9320-96e41aee1b791651484798300-Hrx-_Puma_-_More.jpg"
+  },
+  {
+    name: "Smart Watch",
+    price: "₹2999",
+    img: "https://assets.myntassets.com/f_webp,w_140,c_limit,fl_progressive,dpr_2.0/assets/images/2022/5/2/44192c45-7393-4ede-a926-11f30b0b92a51651484798036-All.jpg"
+  },
+  {
+    name: "Smart Watch",
+    price: "₹2999",
+    img: "https://assets.myntassets.com/f_webp,w_140,c_limit,fl_progressive,dpr_2.0/assets/images/2022/5/2/44192c45-7393-4ede-a926-11f30b0b92a51651484798036-All.jpg"
+  },
+  {
+    name: "Smart Watch",
+    price: "₹2999",
+    img: "https://assets.myntassets.com/f_webp,w_140,c_limit,fl_progressive,dpr_2.0/assets/images/2022/5/2/44192c45-7393-4ede-a926-11f30b0b92a51651484798036-All.jpg"
+  },
+  {
+    name: "Smart Watch",
+    price: "₹2999",
+    img: "https://assets.myntassets.com/f_webp,w_140,c_limit,fl_progressive,dpr_2.0/assets/images/2022/5/2/44192c45-7393-4ede-a926-11f30b0b92a51651484798036-All.jpg"
+  },
+  {
+    name: "Smart Watch",
+    price: "₹2999",
+    img: "https://assets.myntassets.com/f_webp,w_140,c_limit,fl_progressive,dpr_2.0/assets/images/2022/5/2/44192c45-7393-4ede-a926-11f30b0b92a51651484798036-All.jpg"
+  },
+  {
+    name: "Smart Watch",
+    price: "₹2999",
+    img: "https://assets.myntassets.com/f_webp,w_140,c_limit,fl_progressive,dpr_2.0/assets/images/2022/5/2/44192c45-7393-4ede-a926-11f30b0b92a51651484798036-All.jpg"
+  },
+  {
+    name: "Smart Watch",
+    price: "₹2999",
+    img: "https://assets.myntassets.com/f_webp,w_140,c_limit,fl_progressive,dpr_2.0/assets/images/2022/5/2/44192c45-7393-4ede-a926-11f30b0b92a51651484798036-All.jpg"
+  },
+  {
+    name: "Smart Watch",
+    price: "₹2999",
+    img: "https://assets.myntassets.com/f_webp,w_140,c_limit,fl_progressive,dpr_2.0/assets/images/2022/5/2/44192c45-7393-4ede-a926-11f30b0b92a51651484798036-All.jpg"
+  },
+  {
+    name: "Smart Watch",
+    price: "₹2999",
+    img: "https://assets.myntassets.com/f_webp,w_140,c_limit,fl_progressive,dpr_2.0/assets/images/2022/5/2/44192c45-7393-4ede-a926-11f30b0b92a51651484798036-All.jpg"
+  },
+  {
+    name: "Smart Watch",
+    price: "₹2999",
+    img: "https://assets.myntassets.com/f_webp,w_140,c_limit,fl_progressive,dpr_2.0/assets/images/2022/5/2/44192c45-7393-4ede-a926-11f30b0b92a51651484798036-All.jpg"
+  },
+  {
+    name: "Smart Watch",
+    price: "₹2999",
+    img: "https://assets.myntassets.com/f_webp,w_140,c_limit,fl_progressive,dpr_2.0/assets/images/2022/5/2/44192c45-7393-4ede-a926-11f30b0b92a51651484798036-All.jpg"
+  },
+  {
+    name: "Smart Watch",
+    price: "₹2999",
+    img: "https://assets.myntassets.com/f_webp,w_140,c_limit,fl_progressive,dpr_2.0/assets/images/2022/5/2/44192c45-7393-4ede-a926-11f30b0b92a51651484798036-All.jpg"
+  },
+  {
+    name: "Smart Watch",
+    price: "₹2999",
+    img: "https://assets.myntassets.com/f_webp,w_140,c_limit,fl_progressive,dpr_2.0/assets/images/2022/5/2/44192c45-7393-4ede-a926-11f30b0b92a51651484798036-All.jpg"
+  },
+  {
+    name: "Smart Watch",
+    price: "₹2999",
+    img: "https://assets.myntassets.com/f_webp,w_140,c_limit,fl_progressive,dpr_2.0/assets/images/2022/5/2/44192c45-7393-4ede-a926-11f30b0b92a51651484798036-All.jpg"
+  },
+  {
+    name: "Smart Watch",
+    price: "₹2999",
+    img: "https://assets.myntassets.com/f_webp,w_140,c_limit,fl_progressive,dpr_2.0/assets/images/2022/5/2/44192c45-7393-4ede-a926-11f30b0b92a51651484798036-All.jpg"
+  },
+  {
+    name: "Smart Watch",
+    price: "₹2999",
+    img: "https://assets.myntassets.com/f_webp,w_140,c_limit,fl_progressive,dpr_2.0/assets/images/2022/5/2/44192c45-7393-4ede-a926-11f30b0b92a51651484798036-All.jpg"
+  },
+  {
+    name: "Smart Watch",
+    price: "₹2999",
+    img: "https://assets.myntassets.com/f_webp,w_140,c_limit,fl_progressive,dpr_2.0/assets/images/2022/5/2/44192c45-7393-4ede-a926-11f30b0b92a51651484798036-All.jpg"
+  },
+  {
+    name: "Smart Watch",
+    price: "₹2999",
+    img: "https://assets.myntassets.com/f_webp,w_140,c_limit,fl_progressive,dpr_2.0/assets/images/2022/5/2/44192c45-7393-4ede-a926-11f30b0b92a51651484798036-All.jpg"
+  },
+];
+
+const container = document.querySelector(".product-list");
+
+products.forEach(product => {
+  const div = document.createElement("div");
+  div.className = "product";
+  div.innerHTML = `
+    <img src="${product.img}" alt="${product.name}">
+    <h3>${product.name}</h3>
+    <p>${product.price}</p>
+  `;
+  container.appendChild(div);
+});
